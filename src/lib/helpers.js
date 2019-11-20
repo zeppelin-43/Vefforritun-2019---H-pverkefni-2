d6527595efee09b0a51cf6ba303a381c8272dd27
zeppelin-43 <dantgud@gmail.com>
2019-11-20 14:41:48 +0100

    !

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -3,3 +3,25 @@ export function empty(element) {
     element.removeChild(element.firstChild);
   }
 }
+
+/**
+ * Búa til element og aukalega setja börn ef send með
+ *
+ * @param {string} name Nafn á element
+ * @param  {...any} children Börn fyrir element
+ */
+export function el(name, ...children) {
+  const element = document.createElement(name);
+
+  if (Array.isArray(children)) {
+    children.forEach((child) => {
+      if (typeof child === 'string') {
+        element.appendChild(document.createTextNode(child));
+      } else if (child) {
+        element.appendChild(child);
+      }
+    });
+  }
+
+  return element;
+}
--- src/lib/list.js
+++ src/lib/list.js
@@ -3,9 +3,24 @@ import { empty } from './helpers';
 export default class List {
   constructor() {
     this.container = document.querySelector('.list');
+    this.filters = document.querySelectorAll('.filter');
+    this.URL = 'lectures.json'
   }
 
   load() {
     empty(this.container);
   }
+
+  fetchLectures() {
+
+  }
+
+  //TODO Fall til að merkja fyrirlestur sem lokið
+  
+  //TODO Föll fyrir filtera
+
+
+
+
+
 }
